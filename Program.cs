9ec599d99c302c74db36774fe232ca3eb698c9c7
saraa379 <saraa379@yahoo.com>
2020-11-03 19:10:46 +0100

    intial change

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Sarantsetseg Hedenfalk
+ * Inlämnings uppgift C# del 1
+ */
+
+
+using System;
+
+namespace PragueParking1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prague parking!");
+        }
+    }
+}
