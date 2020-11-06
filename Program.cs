204a53109a242dad8df3bf736694c6217b78dd75
saraa379 <saraa379@yahoo.com>
2020-11-06 12:24:07 +0100

    Menu added

--- Program.cs
+++ Program.cs
@@ -11,7 +11,30 @@ namespace PragueParking1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prague parking!");
+            Console.WriteLine("Welcome to Prague parking!");
+
+            int number;
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                //Menu
+                Console.WriteLine("");
+                Console.WriteLine("Please enter the corresponding number from the menu below");
+                Console.WriteLine("");
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine("1. Leave a vehicle for parking");
+                Console.WriteLine("2. Change a vehicle's parking spot");
+                Console.WriteLine("3. Get your vehicle");
+                Console.WriteLine("4. Search for a vehicle");
+                Console.WriteLine("5. Exit the system");
+
+
+                Console.ReadLine();
+
+            }
+
+
         }
     }
 }
